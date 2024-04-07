ffd0d57bfed4c998ba403195658a86fdcbbb6530
Martin Dørum <martid0311@gmail.com>
2024-04-07 22:57:54 +0200

    initial commit

---
+++ pixfmt.js
@@ -0,0 +1,86 @@
+function convertRGB(dest, src, width, height) {
+	let srci = 0;
+	let desti = 0;
+	for (let i = 0; i < width * height; ++i) {
+		dest[desti++] = src[srci++];
+		dest[desti++] = src[srci++];
+		dest[desti++] = src[srci++];
+		dest[desti++] = 255;
+	}
+}
+
+function convertRGBA(dest, src, width, height) {
+	for (let i = 0; i < width * height * 4; ++i) {
+		dest[i] = src[i];
+	}
+}
+
+function convertGreyscale(dest, src, width, height) {
+	let desti = 0;
+	for (let i = 0; i < width * height; ++i) {
+		dest[desti++] = src[i];
+		dest[desti++] = src[i];
+		dest[desti++] = src[i];
+		dest[desti++] = 255;
+	}
+}
+
+function yuvToRGBA(dest, i, y, u, v) {
+	y -= 16;
+	u -= 128;
+	v -= 128;
+	dest[i + 0] = 1.164 * y + 1.596 * v;
+	dest[i + 1] = 1.164 * y - 0.392 * u - 0.813 * v;
+	dest[i + 2] = 1.164 * y + 2.017 * u;
+	dest[i + 3] = 255;
+}
+
+function convertI420(dest, src, width, height) {
+	let uPlane = width * height;
+	let vPlane = uPlane + Math.floor((width * height) / 4);
+
+	for (let y = 0; y < height; ++y) {
+		for (let x = 0; x < width; ++x) {
+			let uv = Math.floor(y / 2) * Math.floor(width / 2) +
+				Math.floor(x / 2);
+
+			yuvToRGBA(
+				dest, (y * width + x) * 4,
+				src[y * width + x],
+				src[uPlane + uv],
+				src[vPlane + uv]);
+		}
+	}
+}
+
+function convertNV12(dest, src, width, height) {
+	let uvPlane = width * height;
+
+	for (let y = 0; y < height; ++y) {
+		for (let x = 0; x < width; ++x) {
+			let uv = Math.floor(y / 2) * width + Math.floor(x / 2) * 2;
+
+			yuvToRGBA(
+				dest, (y * width + x) * 4,
+				src[y * width + x],
+				src[uvPlane + uv],
+				src[uvPlane + uv + 1]);
+		}
+	}
+}
+
+function convertNV21(dest, src, width, height) {
+	let uvPlane = width * height;
+
+	for (let y = 0; y < height; ++y) {
+		for (let x = 0; x < width; ++x) {
+			let uv = Math.floor(y / 2) * width + Math.floor(x / 2) * 2;
+
+			yuvToRGBA(
+				dest, (y * width + x) * 4,
+				src[y * width + x],
+				src[uvPlane + uv + 1],
+				src[uvPlane + uv]);
+		}
+	}
+}
---
+++ script.js
@@ -0,0 +1,71 @@
+let fileInput = document.getElementById("fileInput");
+let widthInput = document.getElementById("widthInput");
+let heightInput = document.getElementById("heightInput");
+let pixfmtinput = document.getElementById("pixfmtInput");
+
+let data = null;
+
+let metaInfo = document.getElementById("metaInfo");
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+function update() {
+	if (!data) {
+		return;
+	}
+
+	let width = parseInt(widthInput.value);
+	let height = parseInt(heightInput.value);
+	let pixfmt = pixfmtInput.value;
+
+	metaInfo.innerText = width + "x" + height + "/" + pixfmt;
+
+	canvas.width = width;
+	canvas.height = height;
+
+	let imageData = ctx.createImageData(width, height);
+
+	if (pixfmt == "rgb") {
+		convertRGB(imageData.data, data, width, height);
+	} else if (pixfmt == "rgba") {
+		convertRGBA(imageData.data, data, width, height);
+	} else if (pixfmt == "greyscale") {
+		convertGreyscale(imageData.data, data, width, height);
+	} else if (pixfmt == "i420") {
+		convertI420(imageData.data, data, width, height);
+	} else if (pixfmt == "nv12") {
+		convertNV12(imageData.data, data, width, height);
+	} else if (pixfmt == "nv21") {
+		convertNV21(imageData.data, data, width, height);
+	} else {
+		metaInfo.innerText += " -- Unknown pixfmt!";
+		return;
+	}
+
+	ctx.putImageData(imageData, 0, 0);
+}
+
+function loadFile(file) {
+	let reader = new FileReader();
+	reader.addEventListener("load", evt => {
+		data = new Uint8Array(evt.target.result);
+		update();
+	});
+	reader.readAsArrayBuffer(file);
+}
+
+fileInput.onchange = evt => {
+	if (evt.target.files.length < 1) {
+		return;
+	}
+
+	loadFile(evt.target.files[0]);
+};
+
+widthInput.onchange = update;
+heightInput.onchange = update;
+pixfmtInput.onchange = update;
+
+if (fileInput.files.length >= 1) {
+	loadFile(fileInput.files[0]);
+}
